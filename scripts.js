0a71deca6ec1f5b73d81b1ab8c7615d246e44303
Olli Lehikoinen <olli.lehikoinen@taaleri.com>
2024-07-01 12:02:17 +0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,148 @@
+function selectNewMeals(amount) {
+    let meals = getRandomMeals(amount)
+    let list = document.getElementById('meals');
+
+    list.innerHTML = "";
+
+    for (let index in meals) {
+        const meal = meals[index]
+        let entry = document.createElement('li');
+        entry.id = meal
+        entry.appendChild(document.createTextNode(meal));
+        createReplaceButton(entry, meal)
+        list.appendChild(entry);
+    }
+}
+
+function replaceMeal(id) {
+    let entry = document.getElementById(id).parentElement
+    entry.innerHTML = ''
+
+    let meals = getRandomMeals(1)
+    let meal = meals[0]
+
+    entry.id = meal
+    entry.appendChild(document.createTextNode(meal));
+    createReplaceButton(entry, meal)
+}
+
+
+
+
+function getRandomMeals(n) {
+    // Shuffle array
+    const shuffled = meals.sort(() => 0.5 - Math.random());
+
+    // Get sub-array of first n elements after shuffled
+    let selected = shuffled.slice(0, n);
+    return selected;
+}
+
+function createReplaceButton(parent, id) {
+    var button = document.createElement('button');
+    button.innerHTML = 'Replace me';
+    button.id = id+'-button'
+    button.onclick = function(){
+        replaceMeal(this.id)
+    }
+    parent.appendChild(button);
+}
+
+const meals = [
+    "Kikhernepasta",
+    "Makaroonilaatikko",
+    "Tofu-nuudeliwokki",
+    "Halloumpasta",
+    "Gnocchivuokajuttu",
+    "Tofu-nuudelikeitto",
+    "Currylinssikeitto",
+    "Porkkana-tomaattirisotto",
+    "Ahvenfileet + perunamuussi + kermaviilikastike",
+    "Täytetyt pitaleivät",
+    "Munakoiso uunipastavuoka",
+    "Punajuurisosekeitto + patonki",
+    "Chili con härkis",
+    "Vegenugetit + riisi",
+    "Bataatti-nachopelti",
+    "Tomaattirisotto",
+    "Linssikeitto + patonki",
+    "Munakoiso uunipasta kikherneillä",
+    "Perunamuusi + kalapuikot + salaatti",
+    "Quornpiirakka",
+    "Vihapullavuoka",
+    "Kasvis-gnocchipannu",
+    "Uunifetapasta",
+    "Salaatti + patonki",
+    "Halloumstroganoff + riisi",
+    "Lohisalaatti",
+    "Uunifeta-perunasalaatti",
+    "Soijarouhelasagne",
+    "Tonnikalasalaatti",
+    "Tortellonisalaatti (kirjasta)",
+    "Quorn-piirakka ja salaatti",
+    "Gnocchipyttipannu (kirjasta)",
+    "Tonnikalavuoka",
+    "Tomaatti-cashewpasta",
+    "Vegekanakastike + riisi",
+    "Tomaattikikhernekeitto + patonki",
+    "Hampparit ja lohkoperunat",
+    "Punajuuripasta ja feta",
+    "Lohipiirakka",
+    "Parsa, kala, perunamuussi, bernaisekastike",
+    "Tortellonivuoka",
+    "Tomaattirisotto ja mozzarella",
+    "Tomaatti-kikhernekeitto",
+    "Hodarivuoka",
+    "Nakit + perunasalaatti",
+    "Sitruunarisotto",
+    "Parsa-sitruunapasta",
+    "Palak paneer",
+    "Tortillat",
+    "Pinaatti-pestopasta",
+    "Kuuma nachodippi",
+    "Peruna, kalapalat ja kermaviilikastike",
+    "Punajuuripasta",
+    "Uuniperunat + tonnikalatäyte",
+    "Fetasalaatti + patonki",
+    "Kalatacot",
+    "Sieni-gnocchipelti",
+    "Munakoisopasta",
+    "Vegekanakiusaus",
+    "Sitruunainen tofu-perunapelti (kirjasta)",
+    "Raviolisalaatti (kirjasta)",
+    "Viikonlopuksi Kuuma nachodippi + nachot (kirjasta)",
+    "Vihapullavuoka (AKlla resepti)",
+    "Paahdettu juures-kikhernesalaatti",
+    "Riisi-halloumvuoka",
+    "Wokki",
+    "Munakoiso-linssivuoka",
+    "Kurpitsatortillat",
+    "Kurpitsarisotto",
+    "Nachopelti",
+    "Kesäkurpitsapasta",
+    "Riisihalloumvuoka",
+    "Pasta bolognese + pasta",
+    "Tofu-korma + riisi",
+    "Pitsa (pesto-mozzarella)",
+    "Texmex piirakka",
+    "Bataatti-kikhernesalaatti",
+    "Halloum-stroganoff",
+    "Halloumwrapit",
+    "Täytetyt paprikat",
+    "Pastasalaatti",
+    "Mozzarella/avocadotaskut",
+    "Kasvispiirakka + salaatti",
+    "Pinaattiletut + puolukkahillo ja salaatti",
+    "Punajuuripasta + tofu ja bimi",
+    "Sitruuna-vuohenjuustorisotto",
+    "Munakoiso-linssibolognese",
+    "Nyhtökaura-gnocchivuoka",
+    "Porkkanapihvit ja perunamuussi",
+    "Tahini-pastasalaatti",
+    "Chili sin carnella täytetyt tortillat",
+    "Meksikolainen tomaattikeitto",
+    "Kasvispiirakka",
+    "Pasta alfredo",
+    "Bao bun sämpylät + itsetehty coleslaw",
+    "Tofu-kookoskorma riisillä"
+];
